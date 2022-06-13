4b9732fc6080d86e9d1f53bcdfda794e160ce158
Liam Hilliard <liam.h.-@hotmail.com>
2022-06-14 00:39:42 +0100

    Initial commit of multi-module project

---
+++ gbserver/src/main/java/com/x04e/guidebook/gbserver/App.java
@@ -0,0 +1,13 @@
+package com.x04e.guidebook.gbserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
